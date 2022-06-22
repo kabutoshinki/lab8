c832c4925519c85b09b93d9de9300148bc4fa8bc
kabutoshinki <kabutoshinki@gmail.com>
2022-06-22 20:51:45 +0700

    React Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 //import logo from './logo.svg';
 import './App.css';
-import React ,{Component} from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+
     );
   }
 }
 
- 
+
 export default App;
